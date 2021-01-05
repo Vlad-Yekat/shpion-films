b7801a0771947f6dd58c1aa13dba78088583f2f4
Vladislav <vladislavi@mail.ru>
2021-01-05 11:59:38 +0500

    Add some models

---
+++ films/core/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from datetime import date
+from django.urls import reverse
+
+from users.models import CustomUser
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="actors/")
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, verbose_name="actors", related_name="film_actor")
+    slug = models.SlugField(max_length=130, unique=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    author = models.ForeignKey(CustomUser,
+                               on_delete=models.deletion.CASCADE,
+                               null=True,
+                               blank=True)
+    name = models.CharField(max_length=100)
+    text = models.TextField(max_length=5000)
+    parent = models.ForeignKey(
+        'self', verbose_name="Parent", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.movie}"
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
