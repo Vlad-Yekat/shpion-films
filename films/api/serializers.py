c3966a7c38d56cdbabfab3279944db10c33b8507
Vladislav <vladislavi@mail.ru>
2021-01-13 10:05:04 +0500

    Add recursive review

--- films/api/serializers.py
+++ films/api/serializers.py
@@ -9,10 +9,26 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ('title', 'description')
 
 
+class RecursiveSerializer(serializers.Serializer):
+    def to_representation(self, instance):
+        serializer = self.parent.parent.__class__(instance, context=self.context)
+        return serializer.data
+
+
+class FilterReviewSerializer(serializers.ListSerializer):
+    """ Only parents """
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
 class ReviewSerializer(serializers.ModelSerializer):
+    children = RecursiveSerializer(many=True)
+
     class Meta:
+        list_serializer_class = FilterReviewSerializer
         model = Review
-        fields = ('author', 'text')
+        fields = ('author', 'text', 'children')
 
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
